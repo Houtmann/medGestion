681fba863ca59121f01dcc68e449685bf6f013f3
Had <hhoutmann@gmail.com>
2018-06-03 14:58:10 +0200

    Implémentatio login et enregistrement

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+/**
+ * @Route("/", name="login")
+ */
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
+
+    {
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }}
